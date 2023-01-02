877ae9c62ed0c279f87feed1b199a79aa56f6e44
jshwi <stephen@jshwisolutions.com>
2023-01-02 11:26:57 +1100

    doc: Document signatures
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- spall/exceptions.py
+++ spall/exceptions.py
@@ -11,7 +11,10 @@ from subprocess import CalledProcessError  # noqa pylint: disable=unused-import
 
 
 class CommandNotFoundError(OSError):
-    """Raise when subprocess called is not on system."""
+    """Raise when subprocess called is not on system.
+
+    :param cmd: Name of command that was not found.
+    """
 
     def __init__(self, cmd: str) -> None:
         super().__init__(f"{cmd}: command not found...")
